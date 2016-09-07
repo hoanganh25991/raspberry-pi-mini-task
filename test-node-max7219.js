872f24bee3ec5ba1ba8691587c1e8a58ccbd9532
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-09-07 19:29:28 +0700

    try repl, then kill process

--- test-node-max7219.js
+++ test-node-max7219.js
@@ -2,4 +2,18 @@ var Max7219 = require('node-max7219');
 
 var max7219 = Max7219();
 
-max7219.drawText('hoang');
+// max7219.drawText('tinker io');
+
+const repl = require('repl');
+
+repl.inject = function(injectedObj){
+	var r = repl.start('>');
+	Object.keys(injectedObj).forEach(function(key){
+		r.context[key] = injectedObj[key];
+	});
+	
+};
+
+repl.inject({
+	max7219: max7219
+});

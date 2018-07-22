6b65c48a8dd1d92c746081c673f02c3d19297d92
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-07-22 21:46:10 +0700

    Fix libs

--- oled/src/index.js
+++ oled/src/index.js
@@ -1,24 +1,23 @@
-const OLED = require('oled-js-pi');
+const OLED = require('rpi-oled');
+const pngtolcd = require('png-to-lcd');
 const font = require('oled-font-5x7');
 
 const opts = {
-  width: 128,
-  height: 64,
-  address: 0x3C
+  width: 128, // screen width
+  height: 64, // screen height
+  address: 0x3C, // Pass I2C address of screen if it is not the default of 0x3C
+  datasize: 16, // Change the amount of bytes sent at once (default 16)
+  device: '/dev/i2c-0', // Pass your i2c device here if it is not /dev/i2c-1
+  microview: true, // set to true if you have a microview display
 };
 
 const oled = new OLED(opts);
 
-// do cool oled things here
-// draws 4 white pixels total
-// format: [x, y, color]
-// oled.drawPixel([
-//   [128, 1, 1],
-//   [128, 32, 1],
-//   [128, 16, 1],
-//   [64, 16, 1]
-// ]);
-
+// Draw image
+// pngtolcd('nyan-cat.png', true, function(err, bitmap) {
+//   oled.buffer = bitmap;
+//   oled.update();
+// });
 
 // sets cursor to x = 1, y = 1
 oled.setCursor(1, 1);
